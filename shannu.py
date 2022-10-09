1e99c68ecfb1c571a581b235004ded874d3f3dcb
Shannunotacoder <109965164+Shannunotacoder@users.noreply.github.com>
2022-10-09 11:57:01 +0530

    Add files via upload

---
+++ shannu.py
@@ -0,0 +1,91 @@
+x ="bunny is good"
+print(x[2:5])
+print(x[-5])
+
+x ="we are playing"
+print(x.upper())
+print(x.lower())
+
+x ="we are studying in lpu"
+print(x.split())
+
+x ="shannu"
+y ="topper"
+
+z =x + " " + y
+print(z)
+
+x ="saannu"
+print(x.replace('a','h'))
+
+print(80 > 58)
+print(67 < 80)
+print(20 == 0)
+
+print(bool('hello'))
+print(bool())
+print(bool[])
+print(bool(0))
+
+mylist = [5,6,7,8,9]
+print(mylist)
+print(mylist[4])
+
+mylist1 = [7,5,4,3,2]
+mylist1[1:2] = [12,13]
+print(mylist1)
+
+mylist1.inset(132,134)
+print(mylist1)
+
+mylist1.append(73)
+print(mylist1)
+
+
+mylist2 = [100.200,300,400]
+mylist3 = [700,800,900,1000]
+mylist2.extend(mylist3)
+print(mylist2)
+print(mylist3)
+
+
+
+# # # python list
+# # # lists are used to store multiple items of either same or different data types into a single variable
+
+# # mylist = [10,70,80,80]
+# # print(mylist)
+# # print(mylist[3])
+# # print(mylist[-])
+# my_list = [10,20,30,40]
+# my_list[1:2] = [200,300]
+# print(my_list)
+
+# my_list.insert(2,500)
+# print(my_list)
+# my_list.append(90)
+# print(my_list)
+
+
+# mylist1 = [10,20,30,40]
+# mylist2 = [50,60,70,80]
+
+# mylist1.extend(mylist2)
+# print(mylist1)
+# print(mylist2)
+
+# mylist = [10,20,30]
+# mytuple = (40,50)
+# mylist.extend(mytuple)
+# print(mylist)
+
+mylist = ['apples' , 'oranges' , 'banana']
+#mylist.remove('banana')
+#print(mylist)
+#mylist.pop(0)
+#print(mylist)
+#del mylist[1]
+#print(mylist)
+mylist.clear()
+print(mylist)
+
